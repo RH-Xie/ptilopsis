14b3c2bf27e666c52483fe10c09ab3c46521d639
RH-Xie <1748421646@qq.com>
2024-01-10 00:23:14 +0800

    try fix: 查询订阅

--- nonebot_bison/sub_manager/__init__.py
+++ nonebot_bison/sub_manager/__init__.py
@@ -27,7 +27,7 @@ add_sub_matcher.handle()(set_target_user_info)
 do_add_sub(add_sub_matcher)
 
 
-query_sub_matcher = on_command("查询订阅", rule=configurable_to_me, priority=5, block=True)
+query_sub_matcher = on_command("查询订阅", '''rule=configurable_to_me''', priority=5, block=True)
 query_sub_matcher.handle()(set_target_user_info)
 do_query_sub(query_sub_matcher)
 
